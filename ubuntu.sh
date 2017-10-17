2f723bec86c904ef100d83f3374f54aebd37d7f5
Kevin Chambers <kc@kevinchambers.com>
2017-10-16 21:54:28 -0700

    Switch Back to i386 arch

--- ubuntu.sh
+++ ubuntu.sh
@@ -12,7 +12,7 @@ if [ "$first" != 1 ];then
         elif [ "$(dpkg --print-architecture)" = "arm" ];then
             wget https://partner-images.canonical.com/core/zesty/current/ubuntu-zesty-core-cloudimg-armhf-root.tar.gz -O ubuntu.tar.gz
         elif [ "$(dpkg --print-architecture)" = "i686" ];then
-            wget https://partner-images.canonical.com/core/zesty/current/ubuntu-zesty-core-cloudimg-amd64-root.tar.gz -O ubuntu.tar.gz
+            wget https://partner-images.canonical.com/core/zesty/current/ubuntu-zesty-core-cloudimg-i386-root.tar.gz -O ubuntu.tar.gz
         else
             echo "unknown architecture"
             exit 1
